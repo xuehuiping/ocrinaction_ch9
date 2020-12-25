525c52f303ba0f475e0b80265d66a930ea696157
xuehuiping <xuehuiping@live.cn>
2020-12-25 08:55:34 +0800

    add MAD

---
+++ MAD.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+"""
+@date: 2020/12/24 10:29 下午
+@author: xuehuiping
+"""
+
+# 离群值检测
+# 代码清单9-3
+
+import numpy as np
+from scipy.stats import norm
+
+
+def detect_outlier(char_probs, thresh=1.4):
+    med = np.median(char_probs)
+    dev = char_probs - med
+    abs_dev = np.absolute(dev)
+    med_abs_dev = np.median(abs_dev)
+    mod_z_score = abs_dev / med_abs_dev * norm.ppf(0.75)
+    error_indices = np.where((mod_z_score > thresh) & (dev < 0))
+    return error_indices[0]
+
+
+if __name__ == "__main__":
+    char_probs = [0.9, 0.3, 0.2, 0.4, 0.5, 0.2, -60]
+    r = detect_outlier(char_probs)
+    print(r)
